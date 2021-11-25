23b41f0447cc1906e888a78d985f65d1f33a5635
taoruntang <rttao@163.com>
2021-11-25 22:51:43 +0800

    TicTacToe

---
+++ src/TicTacToe/Game.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Game extends React.Component{
+    render(){
+        return(
+            <div className="game">
+                <div classname="game-board">
+
+                </div>
+
+                <div className="game-infor">
+                    The game is coming!
+
+                </div>
+
+            </div>
+
+        );
+    }
+}
+
+export default Game;
