712def20a525e7044fbe39a584ee11669b4675eb
taoruntang <rttao@163.com>
2021-11-27 00:16:15 +0800

    TicTacToe v1

--- src/TicTacToe/Game.js
+++ src/TicTacToe/Game.js
@@ -1,15 +1,57 @@
 import React from "react";
+import './Game.css';
+
+class Square extends React.Component{
+    render(){
+        return(
+            <div>
+                <button className="square">{this.props.value}</button>
+            </div>
+        )
+    }
+}
+
+class Board extends React.Component{
+    renderSquare(i){
+        return <Square value={i} />;
+    }
+
+    render(){
+        const hint="Next player:X";
+
+        return(
+            <div>
+                <div>{hint}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
 
 class Game extends React.Component{
     render(){
         return(
             <div className="game">
                 <div classname="game-board">
-
+                    <Board />
                 </div>
 
                 <div className="game-infor">
-                    The game is coming!
+                    游戏即将开始，敬请期待！The game is coming!
 
                 </div>
 
